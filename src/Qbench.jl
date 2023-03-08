c0fc2bdb61d6813566f41885e2cac10bb5d7f189
Ilya Voytov <ilya@voytov.com>
2023-03-08 05:10:46 -0500

    export names

--- src/Qbench.jl
+++ src/Qbench.jl
@@ -3,11 +3,11 @@ module Qbench
 using  BusinessDays, Dates, Statistics, Printf
  
 export Split, Borrow, Security, Close, Trade, Order, TradeType, OrderType, Dividend, Portfolio, Position,
-        adv, isminmarketcap, isminadv, hedgeneed, tradeshareqty, mark!,
+        adv, hedgeneed, tradeshareqty, mark!, fullpositionexpo, getsecurity, hedgeexposure, getfills,
         probable_execution, gettradetype, value, sharesowned, longexposure, shortexposure, equity,
         grossexposure, netexposure, gettrades, cash, cost, nextday!, hedgebook!, positions, pnl,
         getposition, closingbell, shorts, getsecurity, get_missing_nav, refreshdata, annualized_return, sharpe, portfolio_statistics, period_return,
-        returns_by_year, max_drawdown
+        returns_by_year, max_drawdown, TABLE_HEADER, trade_to_target
 
 START_DATE = DateTime(2000,1,1)
 
